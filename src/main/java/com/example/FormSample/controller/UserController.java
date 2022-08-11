ce3d4a2fd0718e408830a9fff8a92f051f8e8ff6
akahori-soma <akahori.soma@gmail.com>
2022-08-11 21:07:08 +0900

    feat formタグ入れ子禁止対策

--- src/main/java/com/example/FormSample/controller/UserController.java
+++ src/main/java/com/example/FormSample/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.example.FormSample.controller;
 
 import com.example.FormSample.entity.User;
+import com.example.FormSample.form.DeleteForm;
 import com.example.FormSample.form.UserForm;
 import com.example.FormSample.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Arrays;
+
+@Transactional(readOnly = false)
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -18,6 +23,7 @@ public class UserController {
     @GetMapping
     public String user(Model model) {
         model.addAttribute("userForm", new UserForm());
+        model.addAttribute("deleteForm", new DeleteForm());
         model.addAttribute("userList",repository.findAll());
         return "user";
     }
@@ -34,6 +40,13 @@ public class UserController {
     @PostMapping("delete")
     public String delete(@RequestParam Integer userId, RedirectAttributes redirectAttributes) {
         repository.deleteById(userId);
+        redirectAttributes.addAttribute("userList",repository.findAll());
+        return "redirect:/user";
+    }
+
+    @PostMapping("deleteAll")
+    public String deleteAll(@ModelAttribute DeleteForm deleteForm, RedirectAttributes redirectAttributes) {
+        repository.deleteAllById(deleteForm.getDeleteList());
         redirectAttributes.addAttribute("userList",repository.findAll());
         return "redirect:/user";
     }
---
+++ src/main/java/com/example/FormSample/form/DeleteForm.java
@@ -0,0 +1,14 @@
+package com.example.FormSample.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeleteForm {
+    private List<Integer> deleteList;
+}
